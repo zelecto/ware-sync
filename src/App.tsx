ac1137b0ef26d20c7dc3fae82c57f6c254592b99
zelecto <ca346364@gmail.com>
2025-11-17 17:26:27 -0500

    feat: add lucide and react-router-dom dependencies; update button component with loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <div className="text-3xl font-bold underline">hello world</div>;
+  return <div className="text-3xl font-bold underline">hello world!</div>;
 }
 
 export default App;
